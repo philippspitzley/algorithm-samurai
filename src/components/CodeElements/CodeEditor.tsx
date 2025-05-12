2f50f1c6528ad552dab0b83701ce497e9c77b4c0
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-05-12 19:56:20 +0200

    ✨ add more functionallity to CodeEditor and improve styling
    - add Run, Submit and Format button
    - add Terminal Output
    - style editor for better integration

--- src/components/CodeElements/CodeEditor.tsx
+++ src/components/CodeElements/CodeEditor.tsx
@@ -2,6 +2,13 @@ import { useEffect, useRef, useState } from "react"
 
 import Editor, { Monaco } from "@monaco-editor/react"
 import { shikiToMonaco } from "@shikijs/monaco"
+import {
+  Ban,
+  Play,
+  SendHorizonal,
+  Sparkles,
+  SquareTerminal,
+} from "lucide-react"
 import { createHighlighter, Highlighter } from "shiki"
 
 import { Button } from "../ui/button"
@@ -12,6 +19,9 @@ const LANGUAGES = ["javascript", "typescript"]
 function CodeEditor() {
   const editorRef = useRef(null)
   const [monaco, setMonaco] = useState<Monaco | null>(null)
+  const [output, setOutput] = useState<string[]>([
+    "WTF, I am running VSCode in my browser 🤩",
+  ])
 
   useEffect(() => {
     async function setHilighter() {
@@ -40,20 +50,70 @@ function CodeEditor() {
     setMonaco(monaco)
   }
 
-  function showValue() {
-    alert(editorRef.current.getValue())
+  function addToTerminal() {
+    setOutput((prev) => [...prev, editorRef.current.getValue()])
+  }
+
+  function resetTerminal() {
+    setOutput([])
+  }
+
+  // Function to trigger code formatting
+  function formatCode() {
+    if (editorRef.current) {
+      // editorRef.current.getAction('editor.action.formatDocument').run();
+      // A more direct way if the action ID is known and you don't need the action object:
+      editorRef.current.trigger(
+        "anyString",
+        "editor.action.formatDocument",
+        null,
+      )
+    }
   }
 
   return (
     <>
-      <Button onClick={showValue}>Get Value</Button>
-      <Editor
-        height="50vh"
-        theme="catppuccin-latte"
-        defaultLanguage={"typescript"}
-        defaultValue={"// this is a comment"}
-        onMount={handleEditorDidMount}
-      />
+      <div className="relative h-80">
+        <Editor
+          height="100%"
+          theme="catppuccin-mocha"
+          defaultLanguage={"typescript"}
+          defaultValue={"// this is a comment"}
+          onMount={handleEditorDidMount}
+          options={{
+            minimap: { enabled: false },
+            scrollbar: { vertical: "hidden" },
+            fontSize: 16,
+            overviewRulerLanes: 0,
+          }}
+        />
+
+        <div className="absolute right-0 bottom-0 flex gap-2">
+          <Button size="icon" variant={"outline"} onClick={formatCode}>
+            <Sparkles />
+          </Button>
+          <Button size="icon" variant={"outline"} onClick={addToTerminal}>
+            <Play />
+          </Button>
+          <Button onClick={addToTerminal}>
+            <SendHorizonal />
+            Submit
+          </Button>
+        </div>
+      </div>
+      <div className="mt-4 ml-6 min-h-16 rounded-lg bg-[#2B2B3C] px-4 py-4 text-left">
+        <div className="flex justify-between">
+          <h3 className="text-md mb-4 flex gap-2 opacity-70">
+            <SquareTerminal size={24} /> Console Output:
+          </h3>
+          <Button size="icon" variant="outline" onClick={resetTerminal}>
+            <Ban size={16} />
+          </Button>
+        </div>
+        {output.map((line) => (
+          <p className="text-violet-300">◉ {line}</p>
+        ))}
+      </div>
     </>
   )
 }
