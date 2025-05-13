8d091b87b9aa5c970931b830c14121d5cd5b4bb1
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-05-13 18:37:50 +0200

    💄 improve styling of Header and fix it on the top of the page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,40 +11,37 @@ function Header() {
   const { logout, isAuthenticated, user } = useAuth()
 
   return (
-    <header className="bg-secondary flex justify-center border-b px-8">
-      <div className="flex w-2xl items-center justify-between py-2">
-        <span className="font-samurai flex-1 translate-y-1 text-3xl uppercase">
-          <Link to="/">AS</Link>
-        </span>
+    <header className="mb-35 flex justify-center px-8">
+      <div className="bg-background/95 border-background fixed z-50 flex w-full items-center justify-between border-2 py-2 backdrop-blur-sm">
+        <div className="mx-auto flex w-full max-w-4xl items-center justify-between">
+          <span className="font-samurai flex-1 translate-y-1 text-3xl uppercase">
+            <Link to="/">AS</Link>
+          </span>
 
-        <div className="flex items-center gap-4">
-          {isAuthenticated && <Button onClick={logout}>Logout</Button>}
-          {!isAuthenticated && (
-            <Button>
-              <Link to="/login">Login</Link>
-            </Button>
-          )}
-          <ThemeToggle />
-        </div>
+          <div className="flex items-center gap-4">
+            {isAuthenticated && <Button onClick={logout}>Logout</Button>}
+            {!isAuthenticated && (
+              <Button>
+                <Link to="/login">Login</Link>
+              </Button>
+            )}
+            <ThemeToggle />
+          </div>
 
-        <div className="flex flex-1 justify-end">
-          <Avatar>
-            <AvatarImage src={AvatarImg} />
-            <AvatarFallback>PS</AvatarFallback>
-          </Avatar>
+          <div className="flex flex-1 justify-end">
+            <Avatar>
+              <AvatarImage src={AvatarImg} />
+              <AvatarFallback>PS</AvatarFallback>
+            </Avatar>
+
+            <Link to="/courses">Courses</Link>
+
+            {isAuthenticated && <Link to={`/users/${user?.id}`}>User</Link>}
+
+            {user?.is_superuser && <Link to="/admin">Admin</Link>}
+          </div>
         </div>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/courses">Courses</Link>
-          </li>
-          <li>
-            {isAuthenticated && <Link to={`/users/${user?.id}`}>User</Link>}
-          </li>
-          <li>{user?.is_superuser && <Link to="/admin">Admin</Link>}</li>
-        </ul>
-      </nav>
     </header>
   )
 }
