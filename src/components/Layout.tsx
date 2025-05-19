931d914ce04c8234e13247f9d810d9598e4360b8
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-05-19 15:14:46 +0200

    💄 improve default layout styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,8 +7,8 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <div className="grid grid-cols-1 bg-[url('/images/third.svg')] bg-cover bg-center bg-no-repeat dark:bg-[url('/images/sixth.svg')]">
-        <main className="container mx-auto mt-5 max-w-4xl min-w-0 px-4 py-8">
+      <div className="h-600 bg-[url('/images/third.svg')] bg-cover bg-center bg-no-repeat dark:bg-[url('/images/sixth.svg')]">
+        <main className="max-w-9xl container mx-auto mt-40 min-w-0 px-4 py-8">
           <Outlet />
         </main>
       </div>
