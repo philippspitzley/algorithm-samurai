9ffe911359db84c1f0a5a88085697632d3668d7e
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-05-27 15:33:11 +0200

    💄 (Layout): add sonner/toast to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 // src/components/Layout.tsx
 import { Outlet } from "react-router-dom"
 
+import { Toaster } from "@/components/ui/sonner"
+
 import Header from "./Header"
 
 const Layout = () => {
@@ -10,6 +12,7 @@ const Layout = () => {
       <div className="h-600 bg-[url('/images/third.svg')] bg-cover bg-center bg-no-repeat dark:bg-[url('/images/sixth.svg')]">
         <main className="max-w-9xl container mx-auto mt-40 min-w-0 px-4 py-8">
           <Outlet />
+          <Toaster />
         </main>
       </div>
     </>
