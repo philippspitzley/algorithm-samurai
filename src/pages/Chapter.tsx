868ab18615c937e3b0aef966dff0a3a3ad44302e
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-05-28 18:35:10 +0200

    ✨ (Chapter): show chapter content or admin editing view
    - if normal user is logged in, show chapter content
    - if admin is logged in, show chapter content with edit button
    - if admin clicks edit button, show Markdown editor with chapter content

--- src/pages/Chapter.tsx
+++ src/pages/Chapter.tsx
@@ -1,49 +1,105 @@
 import { useEffect, useState } from "react"
 
 import { useParams } from "react-router-dom"
+import { toast } from "sonner"
 
 import { getData } from "@/api/fetch"
+import { updateChapter } from "@/api/routes"
 import { getUrl } from "@/api/url-constants"
-import CodeBlock from "@/components/CodeElements/CodeBlock"
+import AdminEditButtons from "@/components/Admin/AdminEditButtons"
 import CodeEditor from "@/components/CodeElements/CodeEditor"
-import { ChapterPoint, CourseChapter } from "@/types/api"
+import Markdown from "@/components/Markdown/Markdown"
+import MarkdownForm from "@/components/Markdown/MarkdownForm"
+import { useAuth } from "@/context/auth/useAuth"
+import { CourseChapter } from "@/types/api"
 
 function Chapter() {
   const { chapterId } = useParams()
   const [chapter, setChapter] = useState<CourseChapter | null>(null)
-  const [chapterPoints, setChapterPoints] = useState<ChapterPoint[] | null>(null)
   const [exercise, setExercise] = useState<string>("")
+  const [adminIsEditing, setAdminIsEditing] = useState(false)
+  const { user } = useAuth()
 
   useEffect(() => {
     if (!chapterId) return
 
     async function getChapter() {
       const chapterUrl = `${getUrl("chapters")}/${chapterId}`
-      const chapter: CourseChapter = await getData(chapterUrl)
+      const chapterData: CourseChapter = await getData(chapterUrl)
 
-      setChapter(chapter)
-      setExercise(chapter?.exercise)
-    }
-
-    async function getChapterPoints() {
-      const pointUrl = `${getUrl("chapters")}/${chapterId}/chapter-points`
-      const chapterPoints: ChapterPoint[] = await getData(pointUrl)
-
-      setChapterPoints(chapterPoints)
+      setChapter(chapterData)
+      setExercise(chapterData?.exercise)
     }
 
     getChapter()
-    getChapterPoints()
   }, [chapterId])
 
-  const chapterPointElements = chapterPoints?.map((point) => (
-    <div key={point.id}>{point?.code_block && <CodeBlock code={point.code_block} />}</div>
-  ))
+  const handleDescriptionUpdate = async (body: { description: string }) => {
+    if (!chapterId) {
+      console.error("Chapter ID is missing.")
+      return
+    }
+    if (!chapter) {
+      console.error("Chapter data not loaded.")
+      return
+    }
+
+    try {
+      // Update database
+      await updateChapter(chapterId, body)
+
+      // Update local state
+      setChapter((prevChapter) => {
+        if (!prevChapter) return null
+        return { ...prevChapter, description: body.description }
+      })
+
+      toast.success("Chapter description updated successfully!")
+    } catch (error) {
+      console.error("Failed to update chapter description:", error)
+      toast.error("Failed to update description. Please try again.")
+    }
+  }
+
+  const showMarkdownElements = () => {
+    if (user?.is_superuser && chapter && adminIsEditing) {
+      return (
+        <MarkdownForm defaultValue={chapter.description} callbackFn={handleDescriptionUpdate} />
+      )
+    } else {
+      return <Markdown markdown={chapter?.description} />
+    }
+  }
+
+  const MarkdownElements = showMarkdownElements()
+
+  const toggleEdit = () => {
+    setAdminIsEditing((prev) => !prev)
+  }
+
+  const handleDelete = () => {
+    toast.error(<div>Chapter successfully deleted!</div>, {
+      action: {
+        label: "Undo",
+        onClick: () => console.log("Undo"),
+      },
+    })
+  }
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="relative flex flex-col gap-4">
       <h1>{chapter?.title}</h1>
-      {chapterPointElements}
-      {exercise && <CodeEditor key={chapter?.id || exercise} defaultValue={exercise} />}
+      <AdminEditButtons
+        isEditing={adminIsEditing}
+        onEdit={toggleEdit}
+        onDelete={handleDelete}
+        className="absolute right-0"
+      />
+
+      {MarkdownElements}
+
+      {exercise && <CodeEditor defaultValue={exercise} />}
+      {/* key={chapter?.id || exercise} Is that necessary for CodeEditor?*/}
     </div>
   )
 }
