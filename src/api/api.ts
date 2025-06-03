baa6d7c36716654808b982190e2bd957481c9e6e
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-03 15:32:37 +0200

    🔧 (api) add fallback host-url and include credentials by default

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,12 +4,14 @@ import createClient from "openapi-react-query"
 import type { paths } from "./openapi-schemas"
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
-const FALLBACK_URL = "http://127.0.0.1:8000"
+const FALLBACK_URL = "http://localhost:8000"
 
 if (BASE_URL === undefined)
   console.info(`"VITE_API_BASE_URL" not found in .env! Fallback: "${FALLBACK_URL}"`)
 
 const fetchClient = createFetchClient<paths>({
-  baseUrl: BASE_URL,
+  baseUrl: BASE_URL || FALLBACK_URL,
+  credentials: "include",
 })
+
 export const $api = createClient(fetchClient)
