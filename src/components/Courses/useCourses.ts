e3d8a4529651070f092b220e991864c51eead3a0
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-05 12:19:05 +0200

    🧱 (Courses): add data fetching hook for a Courses
    - fetching via useQuery and openapi client

---
+++ src/components/Courses/useCourses.ts
@@ -0,0 +1,36 @@
+import { useIsFetching, useQueryClient } from "@tanstack/react-query"
+import { toast } from "sonner"
+
+import { $api } from "@/api/api"
+import { APISchemas } from "@/api/types"
+
+type CourseCreate = APISchemas["CourseCreate"]
+
+function useCourses() {
+  const queryClient = useQueryClient()
+  const isLoading = useIsFetching()
+
+  const { data, error, isError } = $api.useQuery("get", "/api/v1/courses/")
+
+  const { mutate: create } = $api.useMutation("post", "/api/v1/courses/", {
+    onSuccess: () => {
+      toast.success("New Course added.")
+
+      queryClient.invalidateQueries({
+        queryKey: ["get", "/api/v1/courses/"],
+      })
+    },
+    onError: (error) => {
+      toast.error("Something went wrong.", { description: error.detail.message })
+      console.error(error)
+    },
+  })
+
+  const createCourse = (body: CourseCreate) => {
+    create({ body: body })
+  }
+
+  return { data, createCourse, error, isLoading, isError }
+}
+
+export default useCourses
