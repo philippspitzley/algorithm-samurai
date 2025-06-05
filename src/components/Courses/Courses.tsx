28a58b611080399a2b1ca48ca99035080d5e299a
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-05 12:24:39 +0200

    ♻️ (Course): refactor component to use new data fetching hook and forms

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,12 +1,15 @@
 import { toast } from "sonner"
 
-import { $api } from "@/api/api"
 import LoadingSpinner from "@/components/LoadingSpinner"
 
+import AdminContext from "../Admin/AdminContext"
+import { Card } from "../ui/card"
 import Course from "./Course"
+import CreateCourseForm from "./CreateCourseForm"
+import useCourses from "./useCourses"
 
 function Courses() {
-  const { data, error, isLoading, isError } = $api.useQuery("get", "/api/v1/courses/")
+  const { data, error, isLoading, isError, createCourse } = useCourses()
 
   if (isLoading) return <LoadingSpinner />
 
@@ -16,7 +19,12 @@ function Courses() {
   const courseElements = courses?.map((course) => <Course key={course.id} data={course} />)
 
   return (
-    <main className="mt-8 px-4">
+    <main className="mt-8 flex flex-col items-center px-4">
+      <AdminContext className="mb-6 w-full max-w-3xl min-w-xs">
+        <Card className="w-full max-w-3xl min-w-xs px-6">
+          <CreateCourseForm onSubmit={createCourse} />
+        </Card>
+      </AdminContext>
       {courses && <div className="flex flex-col items-center gap-4">{courseElements}</div>}
     </main>
   )
