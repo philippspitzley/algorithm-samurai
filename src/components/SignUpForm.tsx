55fc94a8d97ebfa6d71d91f77e940463d9c69d77
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-10 16:58:14 +0200

    ♻️ (SignUpForm): replace old fetches with react-query

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react"
-
 import { zodResolver } from "@hookform/resolvers/zod"
 import { LoaderCircle } from "lucide-react"
 import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
 import { z } from "zod"
 
-import { ApiError } from "@/api/fetch"
-import { signUp } from "@/api/routes"
+import { $api } from "@/api/api"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -18,35 +17,33 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-import AlertDestructive from "./AlertDestructive"
 import { Card, CardTitle } from "./ui/card"
 
 const formSchema = z
   .object({
-  email: z.string().email({
-    message: "Your email is invalid.",
-  }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters.",
-  }),
-    confirmPassword: z.string(),
-  user_name: z
-    .string()
-    .min(1, {
-      message: "Your name must be at least 1 character.",
-    })
-    .max(20, {
-      message: "Your name can not be longer than 20 characters.",
+    email: z.string().email({
+      message: "Your email is invalid.",
     }),
-})
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+    confirmPassword: z.string(),
+    user_name: z
+      .string()
+      .min(1, {
+        message: "Your name must be at least 1 character.",
+      })
+      .max(20, {
+        message: "Your name can not be longer than 20 characters.",
+      }),
+  })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   })
 
 export function SignUpForm() {
-  const [errorMessage, setErrorMessage] = useState("")
-  const [showAlert, setShowAlert] = useState(false)
+  const navigate = useNavigate()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -58,52 +55,36 @@ export function SignUpForm() {
     },
   })
 
-  const { isSubmitting } = form.formState
+  const signUpMutation = $api.useMutation("post", "/api/v1/users/signup", {
+    onSuccess: () => {
+      toast.success("Sign up successful! Please log in.")
+      navigate("/login")
+    },
+    onError: (error) => {
+      // FIXME: add detail props to the error type
+      // @ts-expect-error detail is not defined in the type
+      toast.error(error?.detail?.message || "Sign up failed")
+    },
+  })
 
-  useEffect(() => {
-    if (errorMessage) {
-      setShowAlert(true)
-      const timer = setTimeout(() => {
-        setShowAlert(false)
-        
-      }, 5000)
-      return () => {
-        clearTimeout(timer)
-      }
-    }
-  }, [errorMessage])
+  const isSubmitting = signUpMutation.isPending
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    setErrorMessage("")
-    try {
-      await signUp(values.email, values.password, values.user_name)
-    } catch (error) {
-      const apiError = error as ApiError
-      const cleanErrorMessage = apiError.message.replace(
-        /^((\p{Extended_Pictographic}\s*){2})/u,
-        "",
-      )
-      setErrorMessage(cleanErrorMessage)
-    }
+    console.log(values)
+
+    signUpMutation.mutate({
+      body: {
+        email: values.email,
+        password: values.password,
+        user_name: values.user_name,
+      },
+    })
   }
 
   return (
     <div className="flex flex-col">
-      {errorMessage && showAlert ? (
-        <div className="bg-background mb-4 rounded-md border p-4 shadow-lg">
-          <AlertDestructive message={errorMessage.slice(1)} />
-          <div className="mt-2 h-0.5 w-full overflow-hidden rounded-full bg-neutral-200 dark:bg-neutral-700">
-            <div className="animate-scale-x-progress h-0.5 origin-left rounded-full bg-red-600 dark:bg-red-500"></div>
-          </div>
-        </div>
-      ) : (
-        <div className="h-32"></div>
-      )}
-
       <Card className="w-lg px-6">
-        <CardTitle className="text-2xl font-semibold">
-          Create a new Account
-        </CardTitle>
+        <CardTitle className="text-2xl font-semibold">Create a new Account</CardTitle>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
@@ -115,11 +96,7 @@ export function SignUpForm() {
                   <FormItem>
                     <FormLabel>User Name</FormLabel>
                     <FormControl>
-                      <Input
-                        placeholder="user@example.com"
-                        {...field}
-                        disabled={isSubmitting}
-                      />
+                      <Input placeholder="Enter your username" {...field} disabled={isSubmitting} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -133,11 +110,7 @@ export function SignUpForm() {
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                      <Input
-                        placeholder="user@example.com"
-                        {...field}
-                        disabled={isSubmitting}
-                      />
+                      <Input placeholder="user@example.com" {...field} disabled={isSubmitting} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -183,16 +156,8 @@ export function SignUpForm() {
               />
             </div>
 
-            <Button
-              type="submit"
-              className={`$ mb-2 w-full`}
-              disabled={isSubmitting}
-            >
-              {isSubmitting ? (
-                <LoaderCircle className="animate-spin" />
-              ) : (
-                "Sign Up"
-              )}
+            <Button type="submit" className="mb-2 w-full" disabled={isSubmitting}>
+              {isSubmitting ? <LoaderCircle className="animate-spin" /> : "Sign Up"}
             </Button>
           </form>
         </Form>
