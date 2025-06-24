edc6df5a3b2563363e4e635bba2b7a9a61375028
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-24 11:20:11 +0200

    ✨ (DeleteButton): add DeleteButton component with confirmation dialog

---
+++ src/components/Admin/DeleteButton.tsx
@@ -0,0 +1,66 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+import ButtonWithTooltip from "../ButtonWithTooltip"
+
+interface Props {
+  name?: string
+  item?: string
+  children?: React.ReactNode
+  className?: string
+  onClick?: () => void
+  size?: "icon" | "default"
+  title?: string
+  variant?: "destructive" | "default"
+  disabled?: boolean
+  asChild?: boolean
+  tooltip: string
+}
+
+export default function DeleteButton({
+  children,
+  onClick: onDelete,
+  className,
+  size,
+  item = "item",
+  title,
+  tooltip,
+}: Props) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <ButtonWithTooltip
+          className={className}
+          size={size}
+          tooltip={tooltip}
+          toolTipColor="red"
+          title={title && tooltip}
+        >
+          {children}
+        </ButtonWithTooltip>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="border-ctp-red/20">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            {`This action cannot be undone. This will permanently delete this ${item} and remove your
+            data from our servers.`}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onDelete}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
