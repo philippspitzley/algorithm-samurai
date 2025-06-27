af9387338da44e2c2e4aab93fcb0e5622a08519c
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-27 14:17:15 +0200

    💄 (button): update button styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,23 +10,20 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground shadow-xs hover:bg-accent",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "hover:bg-destructive hover:text-white border border-destructive/35  shadow-xs focus-visible:ring-destructive/20  dark:focus-visible:ring-destructive/40 dark:bg-input/30 dark:hover:bg-destructive  dark:hover:text-white",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input  dark:hover:bg-accent dark:hover:text-accent-foreground",
+        secondary: "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        ghost: "hover:bg-input/30 hover:text-primary ",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9 hover:scale-110",
+        icon: "size-9",
       },
     },
     defaultVariants: {
