8e32bc005b99c0b217c86ef7806a1d071eeb8c41
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-27 14:57:43 +0200

    ♻️ (alert): add props for additional styling

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,30 @@
+import { Home } from "lucide-react"
 import { Link } from "react-router-dom"
 
+import AlertDestructive from "@/components/AlertDestructive"
 import { Button } from "@/components/ui/button"
 
-function NotFound() {
+interface Props {
+  title?: string
+  message?: string
+}
+
+function NotFound({ title, message }: Props) {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Button>
-        <Link to={"/"}>Back to Home</Link>
+      <AlertDestructive
+        className="justify-items-center"
+        titleStyles="text-2xl text-center"
+        descriptionStyles="text-lg "
+        title={title ? title : "404 - Page Not Found"}
+        message={message ? message : "😢 Sorry the page you are looking for seems not to exist"}
+      />
+
+      <Button asChild className="mt-6">
+        <Link to={"/"}>
+          <Home />
+          Back to Home
+        </Link>
       </Button>
     </div>
   )
