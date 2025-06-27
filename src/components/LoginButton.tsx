aeeb55f98da6112d8b200ed93614e746391ef8d9
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-06-27 14:19:03 +0200

    🧑‍💻 (LoginButton): add button props from standard button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,17 +4,22 @@ import { useAuth } from "@/context/auth/useAuth"
 
 import { Button } from "./ui/button"
 
-interface Props {
+interface Props extends React.ComponentProps<typeof Button> {
   returnToCurrentPage?: boolean
   className?: string
   children?: React.ReactNode
 }
 
-function LoginButton({ returnToCurrentPage = true, className, children = "Login" }: Props) {
+function LoginButton({
+  returnToCurrentPage = true,
+  className,
+  children = "Login",
+  ...buttonProps
+}: Props) {
   const { goToLogin } = useAuth()
 
   return (
-    <Button onClick={() => goToLogin(returnToCurrentPage)} className={className}>
+    <Button onClick={() => goToLogin(returnToCurrentPage)} className={className} {...buttonProps}>
       {children}
     </Button>
   )
