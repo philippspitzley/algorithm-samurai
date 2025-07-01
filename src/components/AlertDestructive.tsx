8502145ce94c63d746fc1c28c599f2cfc56dd469
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-07-01 12:25:58 +0200

    🧑‍💻 (AlertDestructive): add additional props:   - className   - titleStyles   - descriptionStyles   - iconSize

--- src/components/AlertDestructive.tsx
+++ src/components/AlertDestructive.tsx
@@ -1,19 +1,21 @@
-import { AlertCircle } from "lucide-react"
-
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { cn } from "@/lib/utils"
 
 interface Props {
   title?: string
   message?: string
   children?: React.ReactNode
+  className?: string
+  titleStyles?: string
+  descriptionStyles?: string
+  iconSize?: string
 }
 
-function AlertDestructive({ title, message }: Props) {
+function AlertDestructive({ title, message, titleStyles, descriptionStyles, className }: Props) {
   return (
-    <Alert variant="destructive" className="border-destructive/60 text-left">
-      <AlertCircle className="h-4 w-4" />
-      <AlertTitle>{title ? title : "Error"}</AlertTitle>
-      <AlertDescription>{message} </AlertDescription>
+    <Alert variant="destructive" className={cn("border-destructive/60 text-left", className)}>
+      <AlertTitle className={titleStyles}>{title ? title : "Error"}</AlertTitle>
+      <AlertDescription className={descriptionStyles}>{message} </AlertDescription>
     </Alert>
   )
 }
