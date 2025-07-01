16272f83706788fd3bf9f9a6510709964e08f2bb
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-07-01 12:44:28 +0200

    💄 (Header): restyle Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
-import { useAuth } from "@/context/auth/useAuth"
-
-import AvatarImg from "../assets/images/samurai-avatar.jpg"
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
-import { Button } from "./ui/button"
-import { ThemeToggle } from "./ui/theme-toggle"
+import UserMenu from "./UserMenu"
 
 function Header() {
-  const { logout, isAuthenticated, user } = useAuth()
-
   return (
     <header className="flex justify-center px-8">
-      <div className="bg-background fixed z-50 flex w-full items-center justify-between py-2">
+      <div className="bg-background fixed z-50 flex w-full items-center justify-between px-4 py-2">
         <div className="mx-auto flex w-full max-w-4xl items-center justify-between">
           <span className="font-samurai flex-1 translate-y-1 text-3xl uppercase">
             <Link to="/">AS</Link>
           </span>
 
-          <div className="flex items-center gap-4">
-            {isAuthenticated && <Button onClick={logout}>Logout</Button>}
-            {!isAuthenticated && (
-              <Button>
-                <Link to="/login">Login</Link>
-              </Button>
-            )}
-            <ThemeToggle />
+          <div className="flex items-center gap-4 text-lg">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `text-primary/80 border-primary/20 border-b-2 ${isActive ? "text-primary/100 border-primary/100 border-b-2" : ""}`
+              }
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              to="/courses"
+              className={({ isActive }) =>
+                `text-primary/80 border-primary/20 border-b-2 ${isActive ? "text-primary/100 border-primary/100 border-b-2" : ""}`
+              }
+            >
+              Courses
+            </NavLink>
           </div>
 
           <div className="flex flex-1 justify-end">
-            <Avatar>
-              <AvatarImage src={AvatarImg} />
-              <AvatarFallback>PS</AvatarFallback>
-            </Avatar>
-
-            <Link to="/courses">Courses</Link>
-
-            {isAuthenticated && <Link to={`/users/${user?.id}`}>User</Link>}
-
-            {user?.is_superuser && <Link to="/admin">Admin</Link>}
+            <UserMenu />
           </div>
         </div>
       </div>
